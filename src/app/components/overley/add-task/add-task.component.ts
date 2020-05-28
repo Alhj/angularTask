7982960678793b75179c47e4eb7ec5484f2cf92c
ah <ah224cy@student.lnu.se>
2020-05-28 19:15:19 +0200

    working on adding task

--- src/app/components/overley/add-task/add-task.component.ts
+++ src/app/components/overley/add-task/add-task.component.ts
@@ -3,7 +3,11 @@ import { EventEmitter, Output } from '@angular/core';
 import { FormBuilder } from '@angular/forms'
 import { Input } from '@angular/core'
 
+import { genereateTasks } from '../../../helpers/create/create'
+import { updateTask } from '../../../helpers/fetchdata/fetchtask'
+
 import { task, taskCollection } from '../../../models/apiTask/types'
+import { createTask } from '../../../models/types/createTypes'
 
 interface IFbGruop {
   twoCollection: string
@@ -38,7 +42,18 @@ export class AddTaskComponent implements OnInit {
   onClickClose(): void {
     this.closeWindow.emit(true);
   }
-  async onSubmit(): Promise<void> {
+  async onSubmit(taskData: IFbGruop): Promise<void> {
+    const task: task = genereateTasks(taskData.name);
 
+    const data: createTask = {
+      id: this.id,
+      name: taskData.twoCollection,
+      task: task
+    }
+    const addTask: boolean = await updateTask(data);
+
+    if (addTask) {
+      this.closeWindow.emit(true);
+    }
   }
 }
